14bfaea2eb8214bb593dde77190be29f990a11ef
senmunjal <senmunjal@gmail.com>
2020-05-31 17:18:11 +0530

    31-05-2020

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('post/{post}', function ($post) {
+    $posts=[
+        '1'=>"chirag",
+        '2'=>"test"
+    ];
+    return view('welcome',[
+        'post'=>$posts[$post] ?? 'Nothing'
+    ]);
+});
+
+Route::get('/', function () {
+    return view('adduser');
+});
+
+Route::post('save','StudentController@store');
