da1bf4e402a70fccd4ed255a1b1401639ffb2320
senmunjal <senmunjal@gmail.com>
2020-06-01 14:18:24 +0530

    1-06-2020

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,27 @@
-@extends(layout)
+@extends('layout')
 
 @section('content')
-    <div>
-    <p>Hello</p>
-    </div>
+<table class="table  table-inverse ">
+    <thead class="thead-inverse">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($students as $students)
+        <tr>
+                <td>{{$students->id}}</td>
+                <td>{{$students->name}}</td>
+                <td>{{$students->email}}</td>
+                <td><div><a href="user/{{$students->id}}/edit" class="btn btn-warning">Edit</a>
+                <a href="user/{{$students->id}}/delete" class="btn btn-danger">Delete</a></div></td>
+            </tr>
+        
+    @endforeach
+        </tbody>
+</table>
+    
 @endsection
